16314aef889f7918a7a7aea17d8923df6ce761fa
jjtops <94427279+jjtops@users.noreply.github.com>
2023-01-30 21:13:29 -0500

    Update and rename FirstJavaProgram.java to Unit1Lab.java

--- Unit1Lab.java
+++ Unit1Lab.java
@@ -10,7 +10,7 @@ public class FirstJavaProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello All");
-		System.out.println("My name is Jeet Jagtap");
+		System.out.println("My name is");
 		System.out.println("I am 16 years old and my favorite movie is 'godfather' ");
 	}
 
