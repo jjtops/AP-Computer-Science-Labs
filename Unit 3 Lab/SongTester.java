023f8977e960d895d2bccd7442bdeb1e64ec6a69
jjtops <94427279+jjtops@users.noreply.github.com>
2023-01-30 21:17:49 -0500

    Add files via upload

---
+++ Unit 3 Lab/SongTester.java
@@ -0,0 +1,20 @@
+
+/**
+* This class, outputs an example of the 
+
+* a program that will output a song name, artist
+* and the rest of the song by each part of the song
+* 
+*  @JeetJagtap
+*
+*/
+public class SongTester {
+//brings everything together
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Song.printTitle("Piano Man");
+		Song.printAuthor("Bill Joel");
+		Song.printSong();
+	}
+    
+}
