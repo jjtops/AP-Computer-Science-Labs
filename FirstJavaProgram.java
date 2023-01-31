7de3bed4042c8814fd32535cdf0b4e5dcb963fed
jjtops <94427279+jjtops@users.noreply.github.com>
2023-01-30 21:12:42 -0500

    Add files via upload

---
+++ FirstJavaProgram.java
@@ -0,0 +1,17 @@
+/**
+* This class prints the following 3 lines 
+* with an introduction, my name, and my favorite movie
+*  @JeetJagtap
+*
+*/
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello All");
+		System.out.println("My name is Jeet Jagtap");
+		System.out.println("I am 16 years old and my favorite movie is 'godfather' ");
+	}
+
+}
